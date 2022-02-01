56ae5fb91a4061fffbc27d238027a73c6f771e8f
Joanacelle <Joanacelle@hotmail.com>
2022-02-01 19:19:11 -0300

    Ajustes no endpoint categoria id

--- src/main/java/com/joanacelle/cursomc/services/CategoriaService.java
+++ src/main/java/com/joanacelle/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.joanacelle.cursomc.services;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		java.util.Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+		}
 }
