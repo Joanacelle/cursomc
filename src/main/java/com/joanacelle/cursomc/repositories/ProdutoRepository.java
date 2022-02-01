1ca982ab644468ad6b4fbd02a5174bfcb100120c
Joanacelle <Joanacelle@hotmail.com>
2022-02-01 18:41:29 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/joanacelle/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.joanacelle.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joanacelle.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
